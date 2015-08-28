87cc20cabdf27ac3bd6d737009ee6eab5b3ba62d
kasrasadeghi <kasra.sadeghi1@gmail.com>
2015-08-28 12:45:53 -0500

    Initialization
    Downloaded from hub and sent to GitHub

---
+++ src/conditionalstatementspractice/ConditionalStatementsPractice.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conditionalstatementspractice;
+
+
+public class ConditionalStatementsPractice {
+
+    
+    public static void main(String[] args) 
+    {
+        // guardedInfoTable();
+        // linearEquationSolver();
+        // quadraticEquationSolver();
+    }
+    
+    public static void guardedInfoTable()
+    {
+        
+    }
+    
+    public static void linearEquationSolver()
+    {
+        
+    }
+    
+    public static void quadraticEquationSolver()
+    {
+        
+    }
+    
+}
