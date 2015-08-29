37ae324805915318c2d21d1b78e2237c21dc5a0e
kasrasadeghi <kasra.sadeghi1@gmail.com>
2015-08-29 14:05:58 -0500

    2.0
    Finished linearEquationSolver

--- src/conditionalstatementspractice/ConditionalStatementsPractice.java
+++ src/conditionalstatementspractice/ConditionalStatementsPractice.java
@@ -8,14 +8,13 @@ package conditionalstatementspractice;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
 public class ConditionalStatementsPractice {
 
     
     public static void main(String[] args) 
     {
-        guardedInfoTable();
-        // linearEquationSolver();
+        // guardedInfoTable();
+        linearEquationSolver();
         // quadraticEquationSolver();
     }
     
@@ -31,6 +30,20 @@ public class ConditionalStatementsPractice {
         }
         if ( output != -1) return output;
         return inputInteger();     
+    }
+    
+    public static double inputDouble() 
+    {
+        Scanner keyboard = new Scanner( System.in);
+        double output = -1;
+        try {
+            output = keyboard.nextDouble();
+        }
+        catch(InputMismatchException exception) {
+            System.out.println( "The input must be an double.");
+        }
+        if ( output != -1) return output;
+        return inputDouble();     
     }
     
     public static String inputString() 
@@ -81,7 +94,48 @@ public class ConditionalStatementsPractice {
     
     public static void linearEquationSolver()
     {
+      /*TODO:
+        make basic solution using inputDouble
+        account for special case "no solution"
+        account for special case "all reals"
+        */
         
+        System.out.println("Enter the coefficients and constants in the equation"
+                + " Ax + B = Cx + D.");
+        System.out.print("A = ");
+        double A = inputDouble();
+        System.out.print("B = ");
+        double B = inputDouble();
+        System.out.print("C = ");
+        double C = inputDouble();
+        System.out.print("D = ");
+        double D = inputDouble();
+        System.out.println();
+        
+        double solution = (D - B)/(A - C);
+        
+        String signB = "+ ";
+        String signD = "+ ";
+        if (B < 0) signB = "- ";
+        if (D < 0) signD = "- ";
+        System.out.printf( "Solving the equation %.1fx " + signB + "%.1f = %.1fx " 
+                + signD + "%.1f \n\n", A, Math.abs(B), C, Math.abs(D));
+        
+        if (solution == Double.POSITIVE_INFINITY) 
+            System.out.println( "There are no real solutions.");
+        else if (solution == Double.NaN)
+            System.out.println( "The solution is all real numbers.");
+        else System.out.println("The solution is x = " + solution + ".");
+        
+    }
+    
+    public static void guassianEliminationEquationSolver()
+    {
+        //TODO: try to make for fun
+        //TODO: actually use linear algebra
+        // format: "5x + 5y = 4x - 3"
+        // ask for more formulas
+        // simplification code
     }
     
     public static void quadraticEquationSolver()
