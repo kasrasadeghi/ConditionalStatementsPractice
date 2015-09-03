94e80abf507c1c5a1ab7e2332a4cce4d252f6cfc
kasrasadeghi <kasra.sadeghi1@gmail.com>
2015-09-02 22:41:05 -0500

    3.8
    Added the trivial solution and fixed some zeroes.

--- src/conditionalstatementspractice/ConditionalStatementsPractice.java
+++ src/conditionalstatementspractice/ConditionalStatementsPractice.java
@@ -11,8 +11,8 @@ public class ConditionalStatementsPractice {
     public static void main(String[] args) 
     {
         // guardedInfoTable();
-        // linearEquationSolver();
-         quadraticEquationSolver();
+         linearEquationSolver();
+        // quadraticEquationSolver();
     }
     
     public static double inputDouble()
@@ -54,8 +54,7 @@ public class ConditionalStatementsPractice {
     public static void guardedInfoTable()
     {
         System.out.println( "Enter number of people:");
-        int peopleNumber = 0;
-        peopleNumber = inputInt();
+        int peopleNumber = inputInt();
         String[] names = new String[peopleNumber];
         int[] ages = new int[peopleNumber];
         String[] colors = new String[peopleNumber];
@@ -98,6 +97,7 @@ public class ConditionalStatementsPractice {
         System.out.println();
         
         double solution = (D - B)/(A - C);
+        if (solution == 0) solution = 0;
         
         String signB = "+ ";
         String signD = "+ ";
@@ -106,11 +106,15 @@ public class ConditionalStatementsPractice {
         System.out.printf( "Solving the equation %.1fx " + signB + "%.1f = %.1fx " 
                 + signD + "%.1f \n\n", A, Math.abs(B), C, Math.abs(D));
         
-        if (solution == Double.POSITIVE_INFINITY) 
-            System.out.println( "There are no solutions.");
-        else if (solution == Double.NaN)
-            System.out.println( "The solution is all real numbers.");
-        else System.out.println("The solution is x = " + solution + ".");
+        if ( A == C) {
+            if ( B == D) {
+                System.out.println("The solution is all real numbers.");
+            }
+            else {
+                System.out.println("There is no real solution.");
+            }
+        }
+        else System.out.println( "The solution is x = " + solution);
         
     }
     
@@ -128,11 +132,14 @@ public class ConditionalStatementsPractice {
         System.out.printf( "Solving equation %.2fx^2 + %.2fx + %.2f = 0.\n\n", A, B, C);
         if (A == 0) {
             if (B == 0) {
+                if (C == 0) {
+                    System.out.println( "The solution is all real numbers.");
+                }
                 System.out.println( "There are no solutions for this equation.");
                 return;
             }
-            
             double solution = (-C)/B;
+            if (C == 0) solution = 0;
             System.out.println( "This is not a quadratic equation, "
                     + "but the solution is x = " + solution);
             return;
@@ -140,10 +147,11 @@ public class ConditionalStatementsPractice {
         double det = B*B - 4*A*C;
         
         if (det < 0) {
-            det = Math.abs(det);
+            det = -det;
             
             String realComponent = Double.toString((-B)/(2*A));
             String unrealComponent = (Math.sqrt(det)/(2*A)) + "i";
+            if ("-0.0".equals(realComponent)) realComponent = "0.0";
             
             System.out.println( "The solutions are x = " 
                 + realComponent + " + " + unrealComponent + " and x = "
@@ -152,11 +160,14 @@ public class ConditionalStatementsPractice {
         else if (det == 0)
         {
             double solution = (-B)/(2*A);
+            if (solution == 0) solution = 0;
             System.out.println("The solution is " + solution + ".");
         }
         else {
             double upperSolution = (-B + Math.sqrt(det))/(2*A);
             double lowerSolution = (-B - Math.sqrt(det))/(2*A);
+            if (upperSolution == 0) upperSolution = 0;
+            if (lowerSolution == 0) lowerSolution = 0;
             
             System.out.println("The solutions are x = " + upperSolution + " and x = "
             + lowerSolution + ".");
